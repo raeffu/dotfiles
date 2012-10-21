0144dd88c8f156ac9aeaa09d65c76a47d7dca287
raeffu <raeffu@raeffu.ch>
2012-10-21 11:02:52 +0200

    activate rails and org mode for emacs

--- .emacs.d/local.el
+++ .emacs.d/local.el
@@ -1,13 +1,3 @@
 ;; This config file is the last config file loaded. It is useful for local settings
 ;; such as passwords, which you do not want to share with your emacs installations
 ;; on other machines.
-
-;; special keys
-;(cabbage-global-set-key (kbd "<return>") (kbd "RET TAB"))
-;(cabbage-global-set-key (kbd "<f6>") 'theme-roller-cycle)
-
-;; general
-;(global-unset-key (kbd "C-x m")) ; compose-mail
-;(cabbage-global-set-key (kbd "C-x m") 'make-directory)
-;(global-unset-key (kbd "C-<"))
-;(cabbage-global-set-key (kbd "C-<") 'next-buffer)
