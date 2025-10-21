4d4d15998c28cc7e4752d47a90965998f66f18c8
raeffu <raeffu@raeffu.ch>
2025-10-21 14:50:08 +0200

    dimm inactive window

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -9,5 +9,10 @@ return {
 			light = "latte",
 			dark = "macchiato",
 		},
+		dim_inactive = {
+			enabled = true, -- dims the background color of inactive window
+			shade = "dark",
+			percentage = 0.15, -- percentage of the shade to apply to the inactive window
+		},
 	},
 }
