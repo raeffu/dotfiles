d6b3d87d9be344127e0d7058d9be40dbe5ca819c
raeffu <raeffu@raeffu.ch>
2025-12-09 12:43:08 +0100

    format neogit plugin

--- nvim/lua/plugins/neogit.lua
+++ nvim/lua/plugins/neogit.lua
@@ -1,10 +1,10 @@
 return {
-  "NeogitOrg/neogit",
-  dependencies = {
-    "nvim-lua/plenary.nvim", -- required
-    "sindrets/diffview.nvim", -- optional - Diff integration
+	"NeogitOrg/neogit",
+	dependencies = {
+		"nvim-lua/plenary.nvim", -- required
+		"sindrets/diffview.nvim", -- optional - Diff integration
 
-    -- Only one of these is needed.
-    "nvim-telescope/telescope.nvim", -- optional
-  },
+		-- Only one of these is needed.
+		"nvim-telescope/telescope.nvim", -- optional
+	},
 }
